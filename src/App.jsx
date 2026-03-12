35e86ef219dbe8dfdb57ffed36e4888c4743c521
alfaim <hassan.alfaim@gmail.com>
2026-03-13 00:14:28 +0800

    added barebones structure first day working on project. need to fix all the naming issues tmrw, they are old and outdated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import ErrorPage from "./website_pages/ErrorPage";
+import Home from "./website_pages/HomePage";
+import AddTodo from "./website_pages/AddWishlistedGame";
+import useLocalStorage from "use-local-storage";
+import { TodoContext } from "./contexts/WishlistedContext";
+import EditTodo from "./website_pages/EditWishlistedGamesDescription";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Layout() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar bg="light" varient="light">
+        <Container>
+          <Navbar.Brand href="/">Todos</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">Add Todo</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default function App() {
+  const [todos, setTodos] = useLocalStorage("todos", []);
+  return (
+    <TodoContext.Provider value={{ todos, setTodos }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="add" element={<AddTodo />} />
+            <Route path="*" element={<Error page />} />
+            <Route path="todo/:id" element={<EditTodo />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TodoContext.Provider>
+  );
+}
